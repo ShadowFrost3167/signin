a12ccfd9db100d3a8d68a6d9e5e033fa432abf4e
ShadowFrost3167 <olivertwist3167@gmail.com>
2024-06-09 13:56:11 +0300

    neonservererr

--- dbConfig.js
+++ dbConfig.js
@@ -6,14 +6,38 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction ? { rejectUnauthorized: false } : false,
-    idleTimeoutMillis: 30000, // 30 seconds
-    connection: {
-    options: `project=${process.env.ENDPOINT_ID}`,
-  },
-    connectionTimeoutMillis: 2000, // 2 seconds
-});
+// const pool = new Pool({
+//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//     ssl: isProduction ? { rejectUnauthorized: false } : false,
+//     idleTimeoutMillis: 30000, // 30 seconds
+//     connection: {
+//     options: `project=${process.env.ENDPOINT_ID}`,
+//   },
+//     connectionTimeoutMillis: 2000, // 2 seconds
+// });
 
-module.exports = {pool};
+// module.exports = {pool};
+
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+      require: true,
+    },
+  });
+
+  
+  async function getPgVersion() {
+    const client = await pool.connect();
+    try {
+      const result = await client.query('SELECT version()');
+      console.log(result.rows[0]);
+    } finally {
+      client.release();
+    }
+  }
+  getPgVersion();
